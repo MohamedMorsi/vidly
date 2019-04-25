fa477700fa5408da390ab4632671292ee28fb20b
MohamedMorsi <mohamed_morsi94@outlook.com>
2019-04-26 01:51:27 +0200

    Giting start

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            Movie Movie = new Movie() { Name="The Equalizer 1"};
+
+            return View(Movie);
+        }
+    }
+}
